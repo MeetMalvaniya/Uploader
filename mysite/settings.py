29cb8790038aa9d54f71d10b34c021bbe725bc1a
MeetMalvaniya <meetmalvaniya2004@gmail.com>
2025-07-19 01:22:49 +0530

    Fix: updated ALLOWED_HOSTS for Render

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['uploadit.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['uploader-th1h.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
