5bf8440540468ccd737984f799d658805ed2a85a
MeetMalvaniya <meetmalvaniya2004@gmail.com>
2025-07-19 01:30:44 +0530

    Fix: view files page error

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,7 +129,6 @@ MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-MEDIA_URL='/media/'
-
-MEDIA_ROOT=BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
